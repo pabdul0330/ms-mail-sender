4a0fddee0f529597b2618e03d3257bc226fb01f7
pabdu <abdullalipeyman@gmail.com>
2024-05-15 21:33:09 +0400

    first commit

---
+++ src/main/java/com/example/msmailsender/MsMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMailSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMailSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msmailsender/MsMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMailSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
